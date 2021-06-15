552a78fdfccd52410e6e02b51914c51055ac36ce
N Deepika <deepika282002@gmail.com>
2021-06-15 23:35:27 +0530

    mend

--- Day7/assgn.js
+++ Day7/assgn.js
@@ -1,6 +1,30 @@
 //Que1-Write a JavaScript program to list the properties of a JavaScript object. Sample
 // object: var student = { name : "David Rayy", sclass : "VI", rollno : 12 }; Sample Output:
 // name,sclass,rollno
+//sol:-
 
-var student = { name: "David Rayy", sclass: "VI", rollno: 12 };
-console.log(object.keys(student).tostring);
+//var student = { name: "David Rayy", sclass: "VI", rollno: 12 };
+//console.log(Object.keys(student).toString());
+
+//Que2-Write a JavaScript program to delete the rollno property from the following object.
+//Also print the object before or after deleting the property.
+//sol:-
+
+// var student = { name: "David Rayy", sclass: "VI", rollno: 12 };
+// console.log("Before deleting: ", student);
+// delete student.rollno;
+// console.log("After deleting: ", student);
+
+//Que3-Write a JavaScript program to get the length of a JavaScript object. Sample object :
+//sol:-
+
+// var student = { name: "David Rayy", sclass: "VI", rollno: 12 };
+// console.log(Object.keys(student).length);
+
+//Que4-Write a JavaScript program to display the reading status (i.e. display book name,
+//author name and reading status) of the following books. 
+var library = [{ author: 'BillGates', title: 'The Road Ahead', readingStatus: true }, { author: 'Steve Jobs', title: 'WalterIsaacson', readingStatus: true }, { author: 'Suzanne Collins', title: 'Mockingjay: The FinalBook of The Hunger Games', readingStatus: false }];
+console.log(Object.keys(library[0]).toString(), " ");
+for (let i = 0; i < library.length; i++) {
+    console.log(Object.values(library[i]).toString(), " ");
+}
