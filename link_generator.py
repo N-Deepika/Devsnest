3b1442155c235c6f8b5bc13c961282230902c3d1
N Deepika <deepika282002@gmail.com>
2021-06-16 18:55:02 +0530

    Devsnest THA

---
+++ link_generator.py
@@ -0,0 +1,13 @@
+  
+import os
+
+files = ["<h2><b> Devnest Take-Home Assignments </b></h2>\n", "<h4 ><b> @N-Deepika </b></h5>\n"]
+
+for dir, _, _ in os.walk("."):
+	if 'git' not in dir and not dir == ".":
+		name = dir.split("\\")
+		html_link = f"<a href='{dir}'>{name[-1]}</a><br>\n"
+		files.append(html_link)
+	
+with open("index.html", 'w+') as f:
+	f.writelines(files)
