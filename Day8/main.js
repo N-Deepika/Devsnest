3846fbe2e5c70f87f40b43ea5afa7524b7ac5dfe
N Deepika <deepika282002@gmail.com>
2021-06-18 21:42:18 +0530

    DEVSNEST THA

--- Day8/main.js
+++ Day8/main.js
@@ -1,7 +1,7 @@
 let squares = document.getElementsByClassName('class-box');
 
 Array.prototype.forEach.call(squares, (sq) => {
-    sq.addEventListener('mouseover', (event) => {
+    sq.addEventListener('click', (event) => {
         sq.classList.toggle('active');
     })
 });
