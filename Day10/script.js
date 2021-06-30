3d2e9ad87450c240383c4d34e098b2171befff9a
N Deepika <deepika282002@gmail.com>
2021-07-01 00:27:21 +0530

    Devsnest THA

---
+++ Day10/script.js
@@ -0,0 +1,16 @@
+let card = document.getElementsByClassName("card");
+
+// let image = ["dhallows", "dark", "hp", "num", "sortinghat", "specs"];
+// let size = image.length;
+// image = [...image, ...image];
+// const box = document.querySelectorAll('main');
+
+
+// let remainingcards = size * 2;
+// let selection_stack = [];
+// for (let i = 0; i < size * 2; i++) {
+//     let e1 = document.createElement('div');
+//     e1.classList.add('allcards');
+//     box.appendChild(e1);
+
+// }
